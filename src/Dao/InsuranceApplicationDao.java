1f7cce4d150f19cfac26a54729198415d45ef82b
suyeon <ibaesuyeon@Naver.com>
2023-06-03 04:51:44 +0900

    [Fix] DAO수정
    InsuranceApplication DAO SQL문 수정

--- src/Dao/InsuranceApplicationDao.java
+++ src/Dao/InsuranceApplicationDao.java
@@ -16,32 +16,46 @@ public class InsuranceApplicationDao extends Dao{
 		}
 	}
 	public void create(InsuranceApplication insuranceApplication) throws Exception {
-		String query = "insert into InsuranceApplication(applicationID, insuranceID, customerID, createdAt, insurancePeriod, paymentCycle, subscriptionFilePath, premium, maxCompensation, approval, reasonOfApproval) values ('"+
-				insuranceApplication.getApplicationID() + "','" + insuranceApplication.getInsuranceID() + "','" + ",'" + insuranceApplication.getCustomerID() + "','" + insuranceApplication.getCreatedAt() + "','"
-				+ insuranceApplication.getInsurancePeriod() + "','" + insuranceApplication.getPaymentCycle() + "','" + insuranceApplication.getSubscriptionFilePath() + "'," + insuranceApplication.getPremium() + ",'"
-				+ insuranceApplication.getMaxCompensation() + "'," + insuranceApplication.isApproval() + ",'" + insuranceApplication.getReasonOfApproval()+");";
-		System.out.println(query);
+		String query = "INSERT INTO InsuranceApplication (applicationID, insuranceID, customerID, createdAt, insurancePeriod, paymentCycle, subscriptionFilePath, premium, maxCompensation, approval, reasonOfApproval) VALUES ('" +
+				insuranceApplication.getApplicationID() + "','" +
+				insuranceApplication.getInsuranceID() + "','" +
+				insuranceApplication.getCustomerID() + "','" +
+				insuranceApplication.getCreatedAt() + "','" +
+				insuranceApplication.getInsurancePeriod() + "','" +
+				insuranceApplication.getPaymentCycle() + "','" +
+				insuranceApplication.getSubscriptionFilePath() + "'," +
+				insuranceApplication.getPremium() + ",'" +
+				insuranceApplication.getMaxCompensation() + "'," +
+				insuranceApplication.isApproval() + ",'" +
+				insuranceApplication.getReasonOfApproval() + "');";
 		super.create(query);
+
 	}
 	public void createAll(ArrayList<InsuranceApplication> insuranceApplicationList) throws Exception {
 		InsuranceApplication insuranceApplication;
 		for(int i=0;i<=insuranceApplicationList.size();i++) {
 			insuranceApplication = insuranceApplicationList.get(i);
-			String query = "insert into InsuranceApplication(applicationID, insuranceID, customerID, createdAt, insurancePeriod, paymentCycle, subscriptionFilePath, premium, maxCompensation, approval, reasonOfApproval) values ('"+
-					insuranceApplication.getApplicationID() + "','" + insuranceApplication.getInsuranceID() + "','" + ",'" + insuranceApplication.getCustomerID() + "','" + insuranceApplication.getCreatedAt() + "','"
-					+ insuranceApplication.getInsurancePeriod() + "','" + insuranceApplication.getPaymentCycle() + "','" + insuranceApplication.getSubscriptionFilePath() + "'," + insuranceApplication.getPremium() + ",'"
-					+ insuranceApplication.getMaxCompensation() + "'," + insuranceApplication.isApproval() + ",'" + insuranceApplication.getReasonOfApproval()+");";
-			System.out.println(query);
-		super.create(query);
+			String query = "INSERT INTO InsuranceApplication (applicationID, insuranceID, customerID, createdAt, insurancePeriod, paymentCycle, subscriptionFilePath, premium, maxCompensation, approval, reasonOfApproval) VALUES ('" +
+					insuranceApplication.getApplicationID() + "','" +
+					insuranceApplication.getInsuranceID() + "','" +
+					insuranceApplication.getCustomerID() + "','" +
+					insuranceApplication.getCreatedAt() + "','" +
+					insuranceApplication.getInsurancePeriod() + "','" +
+					insuranceApplication.getPaymentCycle() + "','" +
+					insuranceApplication.getSubscriptionFilePath() + "'," +
+					insuranceApplication.getPremium() + ",'" +
+					insuranceApplication.getMaxCompensation() + "'," +
+					insuranceApplication.isApproval() + ",'" +
+					insuranceApplication.getReasonOfApproval() + "');";
+			super.create(query);
 		}
 	}
 	public ArrayList<InsuranceApplication> retrieveAll() throws Exception {
 		String query = "select * from InsuranceApplication;";
-		System.out.println(query);
 		ResultSet results = super.retrieve(query);
-        ArrayList<InsuranceApplication> insuranceApplicationList = new ArrayList<InsuranceApplication>();
+		ArrayList<InsuranceApplication> insuranceApplicationList = new ArrayList<InsuranceApplication>();
 		InsuranceApplication insuranceApplication;
-        while (results.next()){
+		while (results.next()){
 			insuranceApplication = new InsuranceApplication();
 			insuranceApplication.setApplicationID(results.getString("applicationID"));
 			insuranceApplication.setInsuranceID(results.getString("insuranceID"));
@@ -55,30 +69,26 @@ public class InsuranceApplicationDao extends Dao{
 			insuranceApplication.setApproval(results.getBoolean("approval"));
 			insuranceApplication.setReasonOfApproval(results.getString("reasonOfApproval"));
 			insuranceApplicationList.add(insuranceApplication);
-        }  
+		}
 		return insuranceApplicationList;
 	}
 	public void update(InsuranceApplication insuranceApplication) throws Exception {
-		//쿼리 제조
-		String query =  "UPDATE InsuranceApplication SET applicationID = '"+insuranceApplication.getApplicationID()+"', insuranceID = '"+insuranceApplication.getInsuranceID()+"', customerID = "
-				+insuranceApplication.getCustomerID()+", createdAt = '"+insuranceApplication.getCreatedAt()+"', insurancePeriod = '"+insuranceApplication.getInsurancePeriod()+"', "
-				+ "paymentCycle = '"+insuranceApplication.getPaymentCycle()+"', subscriptionFilePath = '"+insuranceApplication.getSubscriptionFilePath()+"', premium = "+insuranceApplication.getPremium()+", "
-				+ "maxCompensation = '"+insuranceApplication.getMaxCompensation()+"', approval = '"+insuranceApplication.isApproval()+"', reasonOfApproval = "+insuranceApplication.getReasonOfApproval()+" WHERE applicationID ='" +insuranceApplication.getApplicationID()+"';";
+		String query = "UPDATE InsuranceApplication SET applicationID = '" + insuranceApplication.getApplicationID() + "', insuranceID = '" + insuranceApplication.getInsuranceID() + "', customerID = '"
+				+ insuranceApplication.getCustomerID() + "', createdAt = '" + insuranceApplication.getCreatedAt() + "', insurancePeriod = '" + insuranceApplication.getInsurancePeriod() + "', "
+				+ "paymentCycle = '" + insuranceApplication.getPaymentCycle() + "', subscriptionFilePath = '" + insuranceApplication.getSubscriptionFilePath() + "', premium = '" + insuranceApplication.getPremium() + "', "
+				+ "maxCompensation = '" + insuranceApplication.getMaxCompensation() + "', approval = '" + insuranceApplication.isApproval() + "', reasonOfApproval = '" + insuranceApplication.getReasonOfApproval() + "' WHERE applicationID = '" + insuranceApplication.getApplicationID() + "';";
 		super.update(query);
 	}
 	public void updateById(String applicationID, String column, String content) throws Exception {
-		String query = "UPDATE InsuranceApplication SET"+column+"="+content+"WHERE applicationID="+applicationID+";";
-		System.out.println(query);
+		String query = "UPDATE InsuranceApplication SET " + column + "='" + content + "' WHERE applicationID='" + applicationID + "';";
 		super.update(query);
 	}
 	public void deleteById(String applicationID) throws Exception {
-		String query = "DELETE FROM InsuranceApplication WHERE applicationID="+applicationID+";";
-		System.out.println(query);
+		String query = "DELETE FROM InsuranceApplication WHERE applicationID='" + applicationID + "';";
 		super.delete(query);
 	}
 	public void deleteAll() throws Exception {
 		String query = "DELETE FROM InsuranceApplication;";
-		System.out.println(query);
 		super.delete(query);
 	}
 }
