567b61985ab0bbd9f12e5916704a2523adbdaba1
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2023-06-07 03:46:10 +0900

    [Fix] Exception

--- src/Exception/CustomConnectException.java
+++ src/Exception/CustomConnectException.java
@@ -1,12 +1,14 @@
 package Exception;
 
-import java.net.ConnectException;
-
-public class CustomConnectException extends ConnectException {
+public class CustomConnectException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	public CustomConnectException(String message) {
 		super(message);
 	}
+
+	public CustomConnectException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
